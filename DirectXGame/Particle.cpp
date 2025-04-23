555becd870b369bb237569256c8f65294fde2553
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2025-04-23 14:52:32 +0900

    CG4_02-02ok

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -1,6 +1,7 @@
 #include "Particle.h"
 
 using namespace KamataEngine;
+using namespace MathUtility;
 
 void Particle::Initialize(Model*model)
 {
@@ -8,16 +9,27 @@ void Particle::Initialize(Model*model)
 
 	model_ = model;
 
+	// 色の設定
+	objectColor_.Initialize();
+	color_ = { 1,1,0,1 };
+
 	worldTransform_.Initialize();
 }
 
 void Particle::Update()
 {
-	worldTransform_.TransferMatrix();
+	// 色変更のオブジェクトに色の数値を設定する
+	objectColor_.SetColor(color_);
+
+	// 移動
+	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+
+	// 行列の更新
+	worldTransform_.UpdateMatrix();
 }
 
 void Particle::Draw(Camera*camera)
 {
 	// 3Dモデルを作成
-	model_->Draw(worldTransform_, *camera);
+	model_->Draw(worldTransform_, *camera,&objectColor_);
 }
