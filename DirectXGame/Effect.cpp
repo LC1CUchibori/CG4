6ae1ea8d42f9f01eff0f274539b5ef7bddf42f36
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2025-05-15 10:44:08 +0900

    CG4_03-04完了

--- DirectXGame/Effect.cpp
+++ DirectXGame/Effect.cpp
@@ -23,6 +23,22 @@ void Effect::Initialize(Model* model,Vector3 scale, Vector3 rotation,Vector3 pos
 
 void Effect::Update()
 {
+
+	// 終了なら何もしない
+	if (IsFinished_) {
+		return;
+	}
+
+	// カウンターを1フレーム分の時間減らす
+	counter_ += 1.0f / 60.0f;
+
+	// 終了時間の上限に達したら
+	if (counter_ >= kDuration) {
+		counter_ = kDuration;
+		// 終了扱いにする
+		IsFinished_ = true;
+	}
+
 	objectColor_.SetColor(color_);
 
 	color_.w = std::clamp(1.0f - counter_ / kDuration, 0.0f, 1.0f);
