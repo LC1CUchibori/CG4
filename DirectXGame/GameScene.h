4533f931da1df7ae16650d24f331889a065b44b2
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2025-07-29 17:05:34 +0900

    ok

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,16 +1,48 @@
 #pragma once
+
 #include "KamataEngine.h"
 
-class GameScene
-{
-public:
-	// 初期化
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
 
-	// 更新
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
 	void Update();
 
-	// 描画
+	/// <summary>
+	/// 描画
+	/// </summary>
 	void Draw();
-};
 
+private: // メンバ変数
+	KamataEngine::DirectXCommon* dxCommon_ = nullptr;
+	KamataEngine::Input* input_ = nullptr;
+	KamataEngine::Audio* audio_ = nullptr;
+
+	std::vector<KamataEngine::Model*> models_;                     // モデルのポインタ配列
+	std::vector<KamataEngine::WorldTransform*> worldTransforms_; 
+
+	KamataEngine::Camera* camera_ = nullptr;
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
